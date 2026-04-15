fd6570d928e1ff5e5502cd69bfc76abab231171c
Miguel Zapata Jiménez <miguel.zapata@ias.com.co>
2026-04-15 08:42:46 -0500

    fix: import observability module in workflow

--- src/workflow/workflow.module.ts
+++ src/workflow/workflow.module.ts
@@ -1,12 +1,13 @@
 import { Module } from "@nestjs/common";
 import { MailModule } from "../mail/mail.module";
+import { ObservabilityModule } from "../observability/observability.module";
 import { OtpModule } from "../otp/otp.module";
 import { TeamsModule } from "../teams/teams.module";
 import { MailPollingService } from "./mail-polling.service";
 import { OtpProcessingService } from "./otp-processing.service";
 
 @Module({
-  imports: [MailModule, OtpModule, TeamsModule],
+  imports: [MailModule, OtpModule, TeamsModule, ObservabilityModule],
   providers: [OtpProcessingService, MailPollingService],
 })
 export class WorkflowModule {}
